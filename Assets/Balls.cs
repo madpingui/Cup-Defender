cb0b53c8bd09dfbd292f7ecea4aaf95756d16c20
Juan Manuel <68016784+madpingui@users.noreply.github.com>
2022-04-15 22:57:11 -0500

    Initial commit

---
+++ Assets/Balls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private float x;
+    private float y = 5;
+
+    public GameObject gameOver;
+
+    void Start()
+    {
+        x = Random.Range(-5, 5);
+        rb.velocity = new Vector2(x, y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            gameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rectangle : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move*speed, 0);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
